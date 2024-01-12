7508d3e326758dcb25048a359fb68784e3730e95
DANGHYEON <rlaalsrl3023@naver.com>
2024-01-12 13:07:13 +0900

    Spring Boot 3.X Security 공부 및 OAuth 공부 ver 0.1

--- src/main/java/com/travel/plan/controller/config/SecurityConfig.java
+++ src/main/java/com/travel/plan/controller/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 
@@ -24,9 +26,14 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/",true)
+                        .loginProcessingUrl("/login/auth")
+                        .usernameParameter("id")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
                         .permitAll()
+
                 )
+
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
