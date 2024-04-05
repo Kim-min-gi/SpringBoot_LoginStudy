5193d81e69ccb8d3c826d12af7862201135a60d2
DANGHYEON <rlaalsrl3023@naver.com>
2024-04-05 19:15:42 +0900

    interceptor 삭제

---
+++ src/main/java/com/travel/plan/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.travel.plan.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+
+}
