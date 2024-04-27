8350dbb41c6f00d82b58cd04c7d031598dae4cef
DANGHYEON <rlaalsrl3023@naver.com>
2024-04-28 07:36:55 +0900

    로그인 구현1

--- src/main/java/com/travel/plan/controller/AuthController.java
+++ src/main/java/com/travel/plan/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.travel.plan.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    @PostMapping("/auth/login")
+    public void login(){
+
+    }
 
 }
