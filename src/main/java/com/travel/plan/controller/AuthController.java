378803972980b3657827714b344f229d63ad25f4
DANGHYEON <rlaalsrl3023@naver.com>
2024-04-29 19:29:40 +0900

    로그인 구현2

--- src/main/java/com/travel/plan/controller/AuthController.java
+++ src/main/java/com/travel/plan/controller/AuthController.java
@@ -1,15 +1,22 @@
 package com.travel.plan.controller;
 
+import com.travel.plan.request.Login;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
     @PostMapping("/auth/login")
-    public void login(){
+    public void login(@RequestBody Login login){
+        log.info(">>>>>login={}", login);
+
+
 
     }
 
